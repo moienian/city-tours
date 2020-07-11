2e1f12c5d057cb0582ba4f476926778d4d9dc26a
Mohammad Moien <mnmoienian@gmail.com>
2020-07-11 12:22:33 +0430

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
 import "./App.scss";
 
 function App() {
-  return <h1>Hello from App</h1>;
+  return <Navbar />;
 }
 
 export default App;
