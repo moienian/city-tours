23551950c646b36bba1d3961b62f3565a1999e76
Mohammad Moien <mnmoienian@gmail.com>
2020-07-12 12:23:55 +0430

    handle remove tour event

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -11,13 +11,14 @@ export default class Tour extends Component {
     });
   };
   render() {
-    const { city, img, name, info } = this.props.tour;
+    const { id, city, img, name, info } = this.props.tour;
+    const { handleRemoveTour } = this.props;
 
     return (
       <article className="tour">
         <div className="img-container">
           <img src={img} alt={name} />
-          <span className="close-btn">
+          <span className="close-btn" onClick={() => handleRemoveTour(id)}>
             <i className="fas fa-window-close"></i>
           </span>
         </div>
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -7,13 +7,27 @@ export default class TourList extends Component {
   state = {
     tours: tourData,
   };
+
+  handleRemoveTour = (id) => {
+    const { tours } = this.state;
+    const sortedTour = tours.filter((tour) => tour.id !== id);
+    this.setState({
+      tours: sortedTour,
+    });
+  };
   render() {
     const { tours } = this.state;
 
     return (
       <section className="tour-list">
         {tours.map((tour) => {
-          return <Tour key={tour.id} tour={tour} />;
+          return (
+            <Tour
+              key={tour.id}
+              tour={tour}
+              handleRemoveTour={this.handleRemoveTour}
+            />
+          );
         })}
       </section>
     );
