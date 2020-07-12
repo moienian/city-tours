b33303c2da45267a17dc3d02d43cde9c1b24ee14
Mohammad Moien <mnmoienian@gmail.com>
2020-07-12 11:43:53 +0430

    link tourdata file to sta te and render city informations

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import Tour from "../Tour/Tour";
 import "./TourList.scss";
+import { tourData } from "../tourData";
 
 export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
   render() {
+    const { tours } = this.state;
+
     return (
       <section className="tour-list">
-        <Tour />
+        {tours.map((tour) => {
+          return <Tour key={tour.id} tour={tour} />;
+        })}
       </section>
     );
   }
